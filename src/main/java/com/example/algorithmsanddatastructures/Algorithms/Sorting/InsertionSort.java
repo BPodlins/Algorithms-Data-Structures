0f8038ee5c8de9d4344890913e08afdfba26c618
Bartosz Podliński <103327465+BPodlins@users.noreply.github.com>
2023-12-05 22:01:25 +0100

    InsertionSort ascending and descending

--- src/main/java/com/example/algorithmsanddatastructures/Algorithms/Sorting/InsertionSort.java
+++ src/main/java/com/example/algorithmsanddatastructures/Algorithms/Sorting/InsertionSort.java
@@ -2,21 +2,34 @@ package com.example.algorithmsanddatastructures.Algorithms.Sorting;
 
 public class InsertionSort {
     public int[] sortAsc(int[] arr){
-        for(int i = 2; i < arr.length; i++){ // i represents the "card" that you want to insert
-            int key = arr[i];
-            int j = i - 1; // j represents the cards that are already after sorting
+        for(int i = 1; i < arr.length; i++){ // i represents the "card" that you want to insert
+            int key = arr[i]; // key represents the number that you are sorting
 
-            while (i>0 && arr[i] > key){
-                arr[i + 1] = arr[i]; // i + 1 are cards that still need to be sorted
-                i = i - 1;
+            int j = i - 1; // j represents the number of positions that are already sorted
+
+            // this loop works while j is bigger than 0, so there are still cards to go through, and number we are currently on
+            // is bigger than the number we want to insert, this is because of the already sorted cards property
+            while (j>0 && arr[j] > key){
+                arr[j + 1] = arr[j]; // we shift the numbers to the right
+                j = j - 1;
             }
-            arr[i+1] = key;
-        }
+            arr[j+1] = key; // when we stop shifting, so when we encounter number smaller than the key, we insert the key
+        }                   // in the next position
         return arr;
     }
 
     public int[] sortDesc(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
 
+            int j = i - 1;
+
+            while (j > 0 && arr[j] < key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
         return arr;
     }
 }
